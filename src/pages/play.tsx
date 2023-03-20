a35b58c624348970fbb4c1473515e04d5e5f37e6
Anshul889 <anshulmanaktala@gmail.com>
2023-03-20 11:03:02 +0530

    google auth and prisma setup

---
+++ src/pages/play.tsx
@@ -0,0 +1,18 @@
+import React, { useState, Fragment } from "react";
+import { Dialog, Transition, Combobox } from "@headlessui/react";
+
+const Play = () => {
+  const [selectedPeople, setSelectedPeople] = useState([]);
+  return (
+    <>
+      <div className="grid grid-cols-4 gap-4 p-4 m-4">
+        <div className="border border-black text-center h-10">bench</div>
+        <div className=" border border-black bg-blue-500"></div>
+        <div className="border border-black"></div>
+        <div className="border border-black"></div>
+      </div>
+    </>
+  );
+};
+
+export default Play;
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@headlessui/tailwindcss')({ prefix: 'ui' })
+  ],
+};
+
+module.exports = config;
