2ac8c5d1d06624d0b74def4564222fb5936d6e94
Anshul889 <anshulmanaktala@gmail.com>
2023-03-21 13:15:58 +0530

    ignore eslint

--- next.config.mjs
+++ next.config.mjs
@@ -25,3 +25,11 @@ const config = {
   },
 };
 export default config;
+
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
