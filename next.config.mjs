c94534d7a3445f001f544ecc28e85622dfbe215c
Anshul889 <anshulmanaktala@gmail.com>
2023-03-21 13:24:04 +0530

    no empty interface rule change

--- next.config.mjs
+++ next.config.mjs
@@ -25,11 +25,3 @@ const config = {
   },
 };
 export default config;
-
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
